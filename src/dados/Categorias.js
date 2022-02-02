49373f9de58a46c4fd7ac3d5fe8b90f4e5b591a7
gabriela.pandini <gabriela.pandini@ambevtech.com.br>
2022-02-02 12:04:19 -0300

    first commit

---
+++ src/dados/Categorias.js
@@ -0,0 +1,27 @@
+export default class Categorias{
+    constructor(){
+        this.categorias = [];
+        this._inscritos = [];
+    }
+
+    inscrever(func){
+        this._inscritos.push(func);
+    }
+
+    desinscrever(func){
+        console.log(this._inscritos.length)
+        this._inscritos = this._inscritos.filter(f => f !== func);
+        console.log(this._inscritos.length)
+    }
+
+    notificar(){
+        this._inscritos.forEach(func =>{
+            func(this.categorias);
+        } );
+    }
+    
+    adicionarCategoria(novaCategoria){
+        this.categorias.push(novaCategoria);
+        this.notificar();
+    }
+}
